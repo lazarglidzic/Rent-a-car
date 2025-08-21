2873cbf20c6245e1a93048b2e8da88388ae3fc20
lazarglidzic <glidzicl@gmail.com>
2025-08-21 14:18:37 +0200

    Add files via upload

---
+++ Unit1.pas
@@ -0,0 +1,33 @@
+unit Unit1;
+
+interface
+
+uses
+  System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants,
+  FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs, FMX.Objects,
+  FMX.TabControl, FMX.Controls.Presentation, FMX.StdCtrls, FMX.Edit;
+
+type
+  TForm1 = class(TForm)
+    Image1: TImage;
+    Label1: TLabel;
+    Edit1: TEdit;
+    Edit2: TEdit;
+    Label2: TLabel;
+    Button1: TButton;
+    Label3: TLabel;
+    Label4: TLabel;
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.fmx}
+
+end.
